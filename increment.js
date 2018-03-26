18024421ffe440503a378f24e4b37e92c00da25e
Stefan Zaharia <stefan.sz490@gmail.com>
2018-03-26 14:47:18 +0300

    readme

--- increment.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
-    $scope.showMe = false;
-    $scope.myFunc = function() {
-        $scope.showMe = !$scope.showMe;
-    }
-});
