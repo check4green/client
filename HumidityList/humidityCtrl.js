3c9e173f4d3a4b142e5db265d570f22f73fd28b2
Stoica <Razvan.Stoica@Honeywell.com>
2018-04-19 15:50:59 +0300

    Update delete and collapse

--- HumidityList/humidityCtrl.js
+++ HumidityList/humidityCtrl.js
@@ -3,32 +3,69 @@
    var app = angular.module("sensorApp");
    app.controller("humidityCtrl", function ($http, $timeout) {
         var vm = this;
+        vm.collapsedRegister = true;
+        vm.collapsedEdit = true;
+        vm.collapsedSensors = true;
+        vm.collapsedMeasurements = true;
+        vm.collapseRegister = function(){
+            if(vm.collapsedRegister == false){
+                vm.collapsedRegister = true;
+                return;
+            };
+            vm.collapsedRegister = false;
+        };
+        vm.collapseEdit = function(){
+            if(vm.collapsedEdit == false){
+                vm.collapsedEdit = true;
+                return;
+            };
+            vm.collapsedEdit = false;
+        };
+        vm.collapseSensors = function(){
+            if(vm.collapsedSensors == false){
+                vm.collapsedSensors = true;
+                return;
+            };
+            vm.collapsedSensors = false;
+        };
+        vm.collapseMeasurements = function(){
+            if(vm.collapsedMeasurements == false){
+                vm.collapsedMeasurements = true;
+                return;
+            };
+            vm.collapsedMeasurements = false;
+        };
         vm.sensorRegister = function(registerProductionDate, registerUploadInterval, registerBatchSize){
             var sensorPost = {'productionDate':registerProductionDate, 'uploadInterval':registerUploadInterval, 'batchSize':registerBatchSize, 'sensorTypeId':"33"}
             $http.post("http://swiss-iot.azurewebsites.net/api/sensors", sensorPost);
         };
         vm.sensorEdit = function(editProductionDate, editUploadInterval, editBatchSize, sensorId){
-            var sensorPut = {'productionDate':editProductionDate, 'uploadInterval':editUploadInterval, 'batchSize':editBatchSize, 'sensorTypeId':"33", id: sensorId, userId:"1"}
+            var sensorPut = {'productionDate':editProductionDate, 'uploadInterval':editUploadInterval, 'batchSize':editBatchSize, 'sensorTypeId':"33", id:sensorId, userId:"1"}
             $http.put("http://swiss-iot.azurewebsites.net/api/sensors/" + sensorId, sensorPut);
         };
-        vm.reload = function(){
-        $http.get("http://swiss-iot.azurewebsites.net/api/sensor-types/33/sensors")
+        $http.get("http://swiss-iot.azurewebsites.net/api/sensor-types/33/sensors?page=0&pageSize=30")
             .then(function(response) {
                 vm.sensors = response.data;
             });
-            $timeout(function(){
-                vm.reload();
-            },500)
-        };
-        vm.reload();
         vm.measurementSensor = function(sensorId){  
             $http.get("http://swiss-iot.azurewebsites.net/api/sensors/" + sensorId + "/readings")
                 .then(function(response) {
                     vm.measurementSensors = response.data;
                 });
         };
-        vm.deleteSensor = function(sensorId){
+        vm.deleting = false;
+        vm.startDelete = function(){
+            vm.deleting = true;
+        }
+        vm.deleteSensor = function(sensorId, sensors, sensor){
+            var idx = sensors.indexOf(sensor);
+            if(idx > -1){
+                sensors.splice(idx, 1);
+            }
             $http.delete("http://swiss-iot.azurewebsites.net/api/sensors/" + sensorId)
+        };
+        vm.cancelDeleteSensor = function(){
+            vm.deleting = false;
         };
     });
 }());
