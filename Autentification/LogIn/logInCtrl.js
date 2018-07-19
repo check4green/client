9246b4be264324b059a18f7760054b184a57512c
unknown <nicu_hd2000@yahoo.com>
2018-07-19 12:52:51 +0300

    Add register-login validation

--- Autentification/LogIn/logInCtrl.js
+++ Autentification/LogIn/logInCtrl.js
@@ -1,27 +1,7 @@
 (function(){
     "use strict";
    var app = angular.module("sensorApp");
-   app.controller("indexCtrl", function ($location, $scope) {
+   app.controller("logInCtrl", function ($scope) {
         var vm = this;
-          $scope.username = "name@yahoo.com";
-          $scope.password = "pass123";
-        $scope.authenticate = function(name, pass){
-          if (name == $scope.username && pass == $scope.password){
-            $location.path('/distance');
-            $scope.nameError = false;
-            $scope.passError = false;
-          } else{
-            if(name != $scope.username){
-              $scope.nameError = true;
-            }else{
-              $scope.nameError = false;
-            }
-            if(pass != $scope.password){
-              $scope.passError = true;
-            }else{
-              $scope.passError = false;
-            }
-          }
-        }
 });
 }())
