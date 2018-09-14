9a7f04de36effb9a269e6d273d50ac4645cd2a18
cristina6diana <cristina.diana05@yahoo.com>
2018-09-14 04:35:14 -0700

    upload interval update

--- SensorModel/detailsSensorDirective.js
+++ SensorModel/detailsSensorDirective.js
@@ -5,7 +5,6 @@ app.directive('detailsSensor', function(){
         templateUrl: 'SensorModel/detailsSensorDirectiveView.html',
         controller: function($scope, sensorModelService, SENSOR_TYPE){
             var vm=this;
-
             $scope.detailsDisplay = true
             $scope.outOfRange = function(sensType){
                 if(sensType == 33){
@@ -31,3 +30,35 @@ app.directive('detailsSensor', function(){
         }
     }
 });
+app.filter("uploadint", function(){
+    return function(input){
+        var seconds = input*60
+        var days= Math.floor(seconds/86400);
+        seconds -= days*86400;
+        var hours = Math.floor(seconds/3600);
+        seconds -= hours*3600;
+        var minutes = Math.floor(seconds/60);
+        if(days !=0 && hours !=0 && minutes !=0){
+            input = days + "d "+ hours + "h " + minutes + "m";
+        }
+        if(days ==0 && hours !=0 && minutes !=0){
+            input = hours + "h " + minutes + "m";
+        }
+        if(days!=0 && hours ==0 && minutes !=0){
+            input = days +"d "+ minutes+"m";
+        }
+        if(days !=0 && hours !=0 && minutes ==0){
+            input =days +"d "+ hours +"h"
+        }
+        if(days ==0 && hours ==0 && minutes !=0){
+            input = minutes +"m";
+        }
+        if(days ==0 && hours!=0 && minutes ==0){
+            input = hours + "h";
+        }
+        if(days !=0 && minutes==0 && hours ==0){
+            input = days +"d"
+        }
+        return input;
+    }
+})
