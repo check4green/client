26e0d367c5631dd8c3afc8a8b153d84a26d257b5
unknown <nicu_hd2000@yahoo.com>
2018-10-15 13:43:31 +0300

    Calculator change

--- Calculator/calculatorDirective.js
+++ Calculator/calculatorDirective.js
@@ -48,50 +48,6 @@ app.directive("calculator", function(){
                         $scope.calculateCost($scope.price5, output5, change5);
                         $scope.calculateTotal();
                         $scope.calculateTotalTwo();
-<<<<<<< HEAD
-                      }
-              $scope.price1 = 94.8;
-              $scope.price2 = 94.8;
-              $scope.price3 = 118.8;
-              $scope.price4 = 94.8;
-              $scope.price5 = 58.8;
-              var change1 =document.getElementById('change1');
-              change1.innerHTML = $scope.price1*output1.innerHTML;
-              var change2 =document.getElementById('change2');
-              change2.innerHTML = $scope.price2*output2.innerHTML;
-              var change3 =document.getElementById('change3');
-              change3.innerHTML = $scope.price3*output3.innerHTML;
-              var change4 =document.getElementById('change4');
-              change4.innerHTML = $scope.price4*output4.innerHTML;
-              var change5 =document.getElementById('change5');
-              change5.innerHTML = $scope.price5*output5.innerHTML;
-              $scope.calculateCost1 = function(){
-                    change1.innerHTML = $scope.price1*output1.innerHTML;
-              }
-              $scope.calculateCost2 = function(){
-                    change2.innerHTML = parseInt($scope.price2*output2.innerHTML);
-              }
-              $scope.calculateCost3 = function(){
-                    change3.innerHTML = parseInt($scope.price3*output3.innerHTML);
-              }
-              $scope.calculateCost4 = function(){
-                    change4.innerHTML =parseInt($scope.price4*output4.innerHTML);
-              }
-              $scope.calculateCost5 = function(){
-                    change5.innerHTML = parseInt($scope.price5*output5.innerHTML);
-              }
-              var total = document.getElementById('total');
-              total.innerHTML =0 ;
-              $scope.calculateTotal = function(){
-                    total.innerHTML = parseInt(change1.innerHTML) + parseInt(change2.innerHTML) + parseInt(change3.innerHTML) + parseInt(change4.innerHTML) + parseInt(change5.innerHTML);
-              }
-              var totalTwo = document.getElementById('totalTwo');
-              totalTwo.innerHTML =0 ;
-              $scope.calculateTotalTwo = function(){
-                    totalTwo.innerHTML = parseInt(change1.innerHTML) + parseInt(change2.innerHTML) + parseInt(change3.innerHTML) + parseInt(change4.innerHTML) + parseInt(change5.innerHTML);
-              }
-        }
-=======
                   }
                   $scope.price1 = 94.8;
                   $scope.price2 = 94.8;
@@ -124,6 +80,5 @@ app.directive("calculator", function(){
                         totalTwo.innerHTML = Math.round(totalRounded2*10) /10;
                   }
             }
->>>>>>> ca95943c0252f250b9d6e9f5db758e6be1c2a9ad
       }
 })
