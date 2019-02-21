2fa918f1b1f0276a92b6836960e99ed2cc193735
cristina6diana <cristina.diana05@yahoo.com>
2019-02-21 14:28:30 +0200

    route update

--- routeCtrl.js
+++ routeCtrl.js
@@ -8,7 +8,7 @@
     });
     app.config(["$stateProvider", "$urlRouterProvider", "$locationProvider", 
                function($stateProvider, $urlRouterProvider, $locationProvider){
-                //$locationProvider.html5Mode(true);
+                $locationProvider.html5Mode(true);
                 $urlRouterProvider.otherwise('/home')
                    $stateProvider
             .state("home",{
